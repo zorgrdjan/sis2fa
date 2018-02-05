ee404c90b3d665b44ae27a549fd70f4d983d317c
zorgrdjan <zoran.grdan@gmail.com>
2018-02-05 01:36:38 +0100

    Created config for connecting on database

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+/* DATABASE CONFIGURATION */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sisprojekt');
+define("BASE_URL", "http://localhost:8012/Sis2fa/"); 
+
+
+function getDB() 
+{
+	$dbhost=DB_SERVER;
+	$dbuser=DB_USERNAME;
+	$dbpass=DB_PASSWORD;
+	$dbname=DB_DATABASE;
+	try {
+	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbConnection->exec("set names utf8");
+	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbConnection;
+    }
+    catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+	}
+
+}
+?>
